2ea1058229297e679b86438d6187c466cfc3d5aa
Reeta Thakur <thakurreeta1995@gmail.com>
2021-08-10 22:07:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/song/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.song;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button playSong;
+    private Button stopSong;
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        playSong=findViewById(R.id.songplay);
+        stopSong=findViewById(R.id.btnstop);
+        playSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent=new Intent(MainActivity.this,MusicService.class);
+                startService(intent);
+
+            }
+        });
+        stopSong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/song/MusicService.java
@@ -0,0 +1,70 @@
+package com.example.song;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.IBinder;
+
+import androidx.core.app.NotificationCompat;
+
+import java.util.Objects;
+
+public class MusicService extends Service {
+    private MediaPlayer mediaPlayer;
+
+    public MusicService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer=MediaPlayer.create(this,R.raw.song);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+    private void showNotificationAndStartForeGround() {
+        createChannel();
+
+        NotificationCompat.Builder notificationBuilder = null;
+        notificationBuilder = new NotificationCompat.Builder(this, "CHANNEL_ID")
+                .setContentTitle("Play Song")
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        Notification notification = null;
+        notification = notificationBuilder.build();
+        startForeground(120, notification);
+    }
+
+
+
+    /*
+Create noticiation channel if OS version is greater than or eqaul to Oreo
+*/
+    public void createChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("CHANNEL_ID", "CHANNEL_NAME", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Call Notifications");
+            Objects.requireNonNull(this.getSystemService(NotificationManager.class)).createNotificationChannel(channel);
+        }
+    }
+}
